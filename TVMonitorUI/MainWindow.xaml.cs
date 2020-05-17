f7715af953727b6eaabd892d31f6f614c0cab629
ericsmithNH <ericsmithnh@gmail.com>
2020-05-17 18:54:37 -0400

    Removing classes

--- TVMonitorUI/MainWindow.xaml.cs
+++ TVMonitorUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TVMonitorGUI
+namespace TVMonitorUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
